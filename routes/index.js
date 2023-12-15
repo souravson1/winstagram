00962562d9e1d168c275d9e0b8f5b1137cbd74fc
Sourav Soni <sauravsoni8980@gmail.com>
2023-12-15 14:33:14 +0530

    initial commit

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var router = express.Router();
+const userModel = require("./users")
+const passport = require('passport');
+const localStrategy = require('passport-local');
+
+passport.use(new localStrategy(userModel.authenticate()))
 
 router.get('/', function(req, res) {
   res.render('index', {footer: false});
@@ -9,24 +14,57 @@ router.get('/login', function(req, res) {
   res.render('login', {footer: false});
 });
 
-router.get('/feed', function(req, res) {
+router.get('/feed', isLoggedIn, function(req, res) {
   res.render('feed', {footer: true});
 });
 
-router.get('/profile', function(req, res) {
+router.get('/profile', isLoggedIn, function(req, res) {
   res.render('profile', {footer: true});
 });
 
-router.get('/search', function(req, res) {
+router.get('/search', isLoggedIn, function(req, res) {
   res.render('search', {footer: true});
 });
 
-router.get('/edit', function(req, res) {
+router.get('/edit', isLoggedIn, function(req, res) {
   res.render('edit', {footer: true});
 });
 
-router.get('/upload', function(req, res) {
+router.get('/upload', isLoggedIn, function(req, res) {
   res.render('upload', {footer: true});
 });
+
+router.post('/register', (req, res, next)=>{
+  const userData = new userModel({
+    username: req.body.username,
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password
+  });
+  userModel.register(userData, req.body.password)
+  .then(function(){
+    passport.authenticate("local")(req, res, function(){
+      res.redirect("/profile");
+    })
+  })
+});
+
+router.post('/login', passport.authenticate("local", {
+  successRedirect: "/profile",
+  failureRedirect: "/login"
+}), function(req, res) {
+});
+
+router.get('/logout', function(req, res, next) {
+  req.logout(function(err) {
+    if (err) { return next(err); }
+    res.redirect('/');
+  });
+});
+
+function isLoggedIn(req, res, next){
+  if(req.isAuthenticated()) return next();
+  res.redirect("/login")
+}
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,17 @@
-var express = require('express');
-var router = express.Router();
+const mongoose = require('mongoose');
+const plm = require('passport-local-mongoose');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+mongoose.connect("mongodb://localhost:27017/instaclone");
 
-module.exports = router;
+const userSchema = mongoose.Schema({
+  username: String,
+  name: String,
+  email: String,
+  password: String,
+  profileImage: String,
+  posts: [{type: mongoose.Schema.Types.ObjectId, ref: "post"}]
+})
+
+userSchema.plugin(plm);
+
+module.exports = mongoose.model("user", userSchema);
