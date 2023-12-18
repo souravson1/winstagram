3938ef62a9cd6cda77c9df54c5dcb09c762c6fac
Sourav Soni <sauravsoni8980@gmail.com>
2023-12-18 10:48:27 +0530

    added likes features on post

---
+++ routes/users.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const plm = require('passport-local-mongoose');
+
+mongoose.connect("mongodb://localhost:27017/instaclone");
+
+const userSchema = mongoose.Schema({
+  username: String,
+  name: String,
+  email: String,
+  password: String,
+  bio: String,
+  profileImage: String,
+  posts: [{type: mongoose.Schema.Types.ObjectId, ref: "post"}]
+})
+
+userSchema.plugin(plm);
+
+module.exports = mongoose.model("user", userSchema);
